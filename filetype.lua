a1f6a473479c2d01f9df01f6e97316f46c915fd8
teddy <teddy@defi.sucks>
2025-09-24 21:43:48 -0300

    dont use filetype.lua as that is not really sourced by packer

--- filetype.lua
+++
@@ -1,9 +0,0 @@
--- local constants = require("noir.constants")
-local extensions = {}
-for _, val in ipairs({"nr","noir"}) do
-	extensions[val] = "noir"
-end
-
-vim.filetype.add({
-	extension = extensions,
-})
--- lua/noir/init.lua
+++ lua/noir/init.lua
@@ -1,9 +1,19 @@
 -- local constants = require("noir.constants")
 local constants = {
 	noir_ctags_group = vim.api.nvim_create_augroup("noir-ctags", {}),
-	noir_patterns = {".noir",".nr"},
+	noir_extensions = {"noir","nr"},
+	noir_patterns = {"*.noir","*.nr"},
 	ctags_cmd={ "ctags", "--langmap=Rust:+(*.noir)", "--langmap=Rust:+(*.nr)" }
 }
+
+local extensions = {}
+for _, val in ipairs(constants.noir_extensions) do
+	extensions[val] = "noir"
+end
+
+vim.filetype.add({
+	extension = extensions,
+})
 
 vim.api.nvim_create_autocmd({ "vimenter" }, {
 	group = constants.noir_ctags_group,
